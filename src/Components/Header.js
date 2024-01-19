22e6ca12acb41b3108ec8c81879c3b556ec1b736
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-01-19 13:26:06 -0500

    Chore: created components folder

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+  return (
+    <ContainerHeader>
+      <h2>Header</h2>
+    </ContainerHeader>
+  )
+}
+
+export default Header;
+
+const ContainerHeader = styled.div`
+    background-color: blue;
+`;
+
+
